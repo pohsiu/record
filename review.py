d568c2dd7d18dbbbe625ea62d0512f83c2ca3f3a
unknown <P15684@P15684-R700-IDS.ideas.iii.org.tw>
2015-11-27 10:57:42 +0800

    nested query and print, load to excel

--- review.py
+++ review.py
@@ -44,7 +44,9 @@ for rows in row:
       r = requests.get(rows[1])
       r_html= r.text.encode('utf8')
       soup = BeautifulSoup(r_html)
-      numberofR = soup.find('span',{'class':'a-size-medium a-text-beside-button totalReviewCount'}).string
+      numberofR = soup.find('span',{'class':'a-size-medium a-text-beside-button totalReviewCount'})
+      if numberofR:
+        numberofR = numberofR.text.encode('utf8').replace(",","")
     except requests.exceptions.ConnectionError:
       r.status_code = "Connection Refused"
   if protect_timeout > 20:
@@ -57,31 +59,96 @@ for rows in row:
     last = num%10
     quotient = num/10
     if quotient is 0:
-     	last = 0
-  	if last > 0:
-    	quotient = quotient + 1
+      last = 0
+    if last > 0:
+      quotient = quotient + 1
         
         
     #===page 1===
     reviews = soup.findAll('div',{'class':'a-section review'})
     for run in reviews:
-      star = <span class="a-icon-alt">
-  		title = <a class="a-size-base a-link-normal review-title a-color-base a-text-bold">
-  		date = <span class="a-size-base a-color-secondary review-date">
-  		writer = <a class="a-size-base a-link-normal author">
-  		contents = <span class="a-size-base review-text">
+      star = run.find('span',{'class':'a-icon-alt'})
+      #<span class="a-icon-alt">
+      title = run.find('a',{'class':'a-size-base a-link-normal review-title a-color-base a-text-bold'})
+      #<a class="a-size-base a-link-normal review-title a-color-base a-text-bold">
+      date = run.find('span',{'class':'a-size-base a-color-secondary review-date'})
+      #<span class="a-size-base a-color-secondary review-date">
+      writer = run.find('a',{'class':'a-size-base a-link-normal author'})
+      #<a class="a-size-base a-link-normal author">
+      contents = run.find('span',{'class':'a-size-base review-text'})
+      #<span class="a-size-base review-text">
+      if star:
+        str_star = star.string
+        print "STAR:"+ str_star
+      if title:
+        str_title = title.string 
+        print "TITLE:"+ str_title
+      if date:
+        str_date = date.string.split('on ')[1]
+        print "DATE:"+ str_date
+      if writer:
+        str_writer = writer.string
+        print "WRITER:"+str_writer
+      if contents:
+        str_contents = contents.text.encode('utf8')
+        print "CONTENTS:"
+        print str_contents
+      sheet.write(index, 0, asin)
+      sheet.write(index, 1, str_star)
+      sheet.write(index, 2, str_title)
+      sheet.write(index, 3, str_date)
+      sheet.write(index, 4, str_writer)
+      sheet.write(index, 5, str_contents)
+      sheet.write(index, 6, row[1])
+      book.save("reviews.xls")
+      index = index + 1
+      print "=================="
     #============
     
     #===if has page 2
     if quotient is 0:
       pass
     else:
-      for page in range(2,quotient+1):
+      for pages in range(2,quotient+1):
+        time.sleep(random.randint(3,5))
         try:
-          r = requests.get(rows[1]+"/ref=cm_cr_pr_btm_link_"+`pages`+"?pageNumber="+`pages`)
+          urlR = rows[1]+"/ref=cm_cr_pr_btm_link_"+`pages`+"?pageNumber="+`pages`
+          r = requests.get(urlR)
         except requests.exceptions.ConnectionError:
           r.status_code = "Connection Refused"
         r_html= r.text.encode('utf8')
         soup = BeautifulSoup(r_html)
-        
+        reviews = soup.findAll('div',{'class':'a-section review'})
+        for run in reviews:
+          star = run.find('span',{'class':'a-icon-alt'})
+          #<span class="a-icon-alt">
+          title = run.find('a',{'class':'a-size-base a-link-normal review-title a-color-base a-text-bold'})
+          #<a class="a-size-base a-link-normal review-title a-color-base a-text-bold">
+          date = run.find('span',{'class':'a-size-base a-color-secondary review-date'})
+          #<span class="a-size-base a-color-secondary review-date">
+          writer = run.find('a',{'class':'a-size-base a-link-normal author'})
+          #<a class="a-size-base a-link-normal author">
+          contents = run.find('span',{'class':'a-size-base review-text'})
+          #<span class="a-size-base review-text">
+          if star:
+            print "STAR:"+star.string
+          if title:
+            print "TITLE:"+title.string
+          if date:
+            print "DATE:"+date.string
+          if writer:
+            print "WRITER:"+writer.string
+          if contents:
+            print "CONTENTS:"
+            print contents.text.encode('utf8')
+          sheet.write(index, 0, asin)
+          sheet.write(index, 1, str_star)
+          sheet.write(index, 2, str_title)
+          sheet.write(index, 3, str_date)
+          sheet.write(index, 4, str_writer)
+          sheet.write(index, 5, str_contents)
+          sheet.write(index, 6, urlR)
+          book.save("reviews.xls")
+          index = index + 1
+          print "=================="
     
