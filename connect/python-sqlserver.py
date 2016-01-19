0b64ca4f0fd6fd5e10331f8c6f714f5b9e41478c
Louis <pohsiu0709@gmail.com>
2016-01-19 19:50:56 +0800

    Update python-sqlserver.py

--- connect/python-sqlserver.py
+++ connect/python-sqlserver.py
@@ -2,9 +2,9 @@ import pyodbc
 #use python pyodbc module to connect sql server
 #module pyodbc installed by 'pip install pyodbc'
 
-cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=P15684-R700-IDS\SQLEXPRESS;DATABASE=CrawlerDB;UID=james;PWD=james123!')
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=adminID\SQLEXPRESS;DATABASE=db;UID=root;PWD=root')
 cursor = cnxn.cursor()
-cursor.execute('select TOP 10 Asin FROM Amazon')
+cursor.execute('select TOP 10 Asin FROM tablename')
 rows = cursor.fetchall()
 for row in rows:
   print row.Asin
